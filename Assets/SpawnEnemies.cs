52d13a6b817334e0de496957c5cb1648df183ed6
Brandon Black <bblack118@gmail.com>
2019-10-17 15:29:17 -0600

    Initail AI Spawn Commit
    Enemies Will spawn. Once we have the "playing field" completed, we'll just adjust a couple values to get proper spawn points.
    Enemies are currently MockPrefabs. Easily Replaced.

---
+++ Assets/SpawnEnemies.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject sprinter;
+    public GameObject average;
+    public GameObject bombardeer;
+
+    private GameObject enemyToSpawn;
+
+    float randY;
+    Vector2 whereToSpawn;
+
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            enemyToSpawn = determineEnemy();
+            nextSpawn = Time.time + spawnRate;
+            randY = Random.Range(-10f, 10f);
+            whereToSpawn = new Vector2(10, randY);
+            Instantiate(enemyToSpawn, whereToSpawn, Quaternion.identity);
+        }
+        
+    }
+
+    GameObject determineEnemy()
+    {
+        int enemySpawnId = Random.Range(0, 100);
+        if (enemySpawnId <= 50)
+            return average;
+        else if (enemySpawnId > 50 && enemySpawnId <= 85)
+            return sprinter;
+        else
+            return bombardeer;
+
+    }
+}
