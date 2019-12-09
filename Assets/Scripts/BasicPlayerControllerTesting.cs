75c8588c0a44113411e1be8ae212235dac3ae9c5
Tyler R Everett <47550163+TylerEverett@users.noreply.github.com>
2019-12-08 22:12:39 -0600

    commit

--- Assets/Scripts/BasicPlayerControllerTesting.cs
+++ Assets/Scripts/BasicPlayerControllerTesting.cs
@@ -5,24 +5,7 @@ using UnityEngine;
 public class BasicPlayerControllerTesting : MonoBehaviour
 {
     public float speed;
-
-    private Rigidbody rb;
-    private Vector3 moveVelocity;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update() 
-    {
-        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
-    }
-
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
-    }
+    public GameObject bullet;
+    public Transform firePoint;
+    public 
 }
