d1696e76f4dabf80191595072b66b95221bb20c5
Tyler R Everett <47550163+TylerEverett@users.noreply.github.com>
2019-12-08 23:19:15 -0600

    Player Controller commit

--- Assets/Scripts/BasicPlayerControllerTesting.cs
+++ Assets/Scripts/BasicPlayerControllerTesting.cs
@@ -2,10 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class BasicPlayerControllerTesting : MonoBehaviour
 {
-    public float speed;
+    public float Speed;
     public GameObject bullet;
     public Transform firePoint;
-    public 
+    public float fireRate;
+    public Boundary boundary;
+
+    private float delayFire;
+    
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.deltaTime > delayFire)
+        {
+            Debug.Log("Pew Pew");
+            delayFire = Time.time + fireRate;
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * Speed;
+
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+         );
+    }
 }
