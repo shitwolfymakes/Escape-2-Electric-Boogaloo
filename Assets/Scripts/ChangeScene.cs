fd9d8bcfba6206cc0fa6340a1075598254c3f5aa
Ashley Abernathy <vkw609@my.utsa.edu>
2019-12-08 20:06:44 -0600

    Ui scripts

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    void Awake()
+    {
+        // To show cursor after First Person Controller
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("LevelUI");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void changeHelp()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void loadHonestJohn()
+    {
+        SceneManager.LoadScene("BobShop");
+    }
+
+    public void loadMadamCannon()
+    {
+        SceneManager.LoadScene("MadamCannon");
+    }
+
+
+}
