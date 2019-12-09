dc558e43942138ade00ad3a3b2da2daa9358b2c6
wolfy <bhawk11@gmail.com>
2019-12-09 13:01:07 -0600

    Major Changes

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -10,24 +10,38 @@ public class SpawnEnemies : MonoBehaviour
     public GameObject bombardeer;
 
     private GameObject enemyToSpawn;
+    private bool spawning;
 
     float randZ;
     Vector3 whereToSpawn;
 
     public float spawnRate = 2f;
     float nextSpawn = 0.0f;
+    float levelLength;
 
     // Start is called before the first frame update
     void Start()
     {
+        levelLength = 7.5f + 30f + (10 * GameManager.Level);
+        StartCoroutine("WaitToSpawn");
+    }
 
+    IEnumerator WaitToSpawn()
+    {
+        //wait for the first part of the song to finish before starting the spawning
+        yield return new WaitForSeconds(7.5f);
+        spawning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Time.time > nextSpawn)
+        if (Time.time > levelLength)
+        {
+            ChangeScene cs = new ChangeScene();
+            cs.loadHonestJohn();
+        }
+        if (Time.time > nextSpawn && spawning)
         {
             enemyToSpawn = determineEnemy();
             nextSpawn = Time.time + spawnRate;
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -5,10 +5,16 @@ using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
+    [Header("Labels")]
     public Text moneyLabel;
     public Text scoreLabel;
-    [SerializeField] GameObject[] ShieldIcons;
+
+    [Header("Icons")]
     [SerializeField] GameObject[] HullIcons;
+    [SerializeField] GameObject[] ShieldIcons;
+    [SerializeField] GameObject HealthBoostIcon;
+    [SerializeField] GameObject ShieldBoostIcon;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +33,7 @@ public class UIScript : MonoBehaviour
         moneyLabel.text = "Money: " + GameManager.Money;
         scoreLabel.text = "Score: " + GameManager.Score;
         UpdateIcons(ShieldIcons, GameManager.Shields);
-        UpdateIcons(HullIcons, GameManager.Shields);
+        UpdateIcons(HullIcons, GameManager.Hull);
     }
 
     void UpdateIcons(GameObject[] GOs, int count)
@@ -35,7 +41,7 @@ public class UIScript : MonoBehaviour
         int i = 0;
         foreach(GameObject go in GOs)
         {
-            Debug.Log(count);
+            //Debug.Log(count);
             if (i < count)
             {
                 go.SetActive(true);
@@ -45,6 +51,25 @@ public class UIScript : MonoBehaviour
                 go.SetActive(false);
             }
             i++;
+        }
+
+        if (GameManager.NumHealthBoosts > 0)
+        {
+            HealthBoostIcon.SetActive(true);
+        }
+        else
+        {
+
+            HealthBoostIcon.SetActive(false);
+        }
+
+        if (GameManager.NumShieldBoosts > 0)
+        {
+            ShieldBoostIcon.SetActive(true);
+        }
+        else
+        {
+            ShieldBoostIcon.SetActive(false);
         }
     }
 }
