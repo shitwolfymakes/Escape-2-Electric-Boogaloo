d4c3a3af2f8fe999d23d0f04354855e8d6037999
wolfy <bhawk11@gmail.com>
2019-12-09 10:51:15 -0600

    starting to hook up UI

---
+++ Assets/UIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Text moneyLabel;
+    public Text scoreLabel;
+    [SerializeField] GameObject[] ShieldIcons;
+    [SerializeField] GameObject[] HullIcons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyLabel.text = "Money: " + GameManager.Money;
+        scoreLabel.text = "Score: " + GameManager.Score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyLabel.text = "Money: " + GameManager.Money;
+        scoreLabel.text = "Score: " + GameManager.Score;
+    }
+}
